47d5e0ec103b01b32f8a3116bf53d42735bfd59d
horizonkitsune <dylan.fouilloux2@gmail.com>
2026-04-10 02:53:44 +0200

    modification keyboard

--- kernel/drivers/keyboard/keyboard.c
+++ kernel/drivers/keyboard/keyboard.c
@@ -5,12 +5,12 @@
 static char scancode_ascii(uint8_t scancode) {
     char table[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
                     'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
-    char table_shift[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
-                    'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
-    char table_tab_lock[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
-                    'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
-    char table_altgr[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
-                    'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
+    //char table_shift[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
+    //                'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
+    //char table_tab_lock[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
+    //                'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
+    //char table_altgr[] = {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ')', '=', 0, 0, 'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '^', '$', '\n', 0, 
+    //                'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 0, 0, 0, '*', 'w', 'x', 'c', 'v', 'b', 'n', ',', ';', ':', '!', 0, 0, 0, 0, ' '}; //tableau scancode -> touche
     if (scancode) {
         return table[scancode];
     }
@@ -25,6 +25,7 @@ void keyboard_key(void) {
     if (scancode & 0x80) return; //fais en sorte d'avoir 1 fois cette touche
     char a = scancode_ascii(scancode); //récupère le carctère correspondant au scancode
     if (a) vga_putchar(a); //affichage du caractère voulu
+    outb(0x20, 0x20); // EOI
 }
 
 
